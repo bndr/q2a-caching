fabf939ba5fe1e8f480d149d0980f04a72f7797c
sama55 <sama55@cmsbox.jp>
2015-07-21 14:21:20 +0900

    Update qa-caching-event.php

--- qa-plugin/q2a-caching/qa-caching-event.php
+++ qa-plugin/q2a-caching/qa-caching-event.php
@@ -7,11 +7,13 @@ require_once QA_PLUGIN_DIR.'q2a-caching/qa-caching-main.php';
 
 class qa_caching_event {
     function process_event ($event, $userid, $handle, $cookieid, $params) {
-        $events = QA_CACHING_EXPIRATION_EVENTS;
-        $events = explode(',', str_replace(array("\r\n", "\r", "\n", " "), '', $events));
-        if(in_array($event, $events)) {
-            $main = new qa_caching_main;
-            $main->clear_cache();
+        if(QA_CACHING_STATUS && !qa_is_logged_in()) {
+            $events = QA_CACHING_EXPIRATION_EVENTS;
+            $events = explode(',', str_replace(array("\r\n", "\r", "\n", " "), '', $events));
+            if(in_array($event, $events)) {
+			    $main = new qa_caching_main;
+                $main->clear_cache();
+            }
         }
     }
 }
