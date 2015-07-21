a80978d4848019d40dae9500e4a745badf06da70
sama55 <sama55@cmsbox.jp>
2015-07-21 14:22:47 +0900

    Create qa-caching-layer.php

---
+++ qa-plugin/q2a-caching/qa-caching-layer.php
@@ -0,0 +1,20 @@
+<?php
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
+require_once QA_PLUGIN_DIR.'q2a-caching/qa-caching-main.php';
+
+class qa_html_theme_layer extends qa_html_theme_base {
+    function head_script() {
+        qa_html_theme_base::head_script();
+        if(QA_CACHING_STATUS && !qa_is_logged_in()) {
+            $this->output('<script src="'.qa_path('qa-plugin/q2a-caching/js/qa-caching.js', null, null, QA_URL_FORMAT_NEAT).'"></script>');
+			
+        }
+    }
+}
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
