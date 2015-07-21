05fecea162f9c30bd5151531532d10e8e402f010
sama55 <sama55@cmsbox.jp>
2015-07-21 16:57:33 +0900

    Update qa-caching-layer.php

--- qa-plugin/q2a-caching/qa-caching-layer.php
+++ qa-plugin/q2a-caching/qa-caching-layer.php
@@ -7,7 +7,8 @@ require_once QA_PLUGIN_DIR.'q2a-caching/qa-caching-main.php';
 
 class qa_html_theme_layer extends qa_html_theme_base {
     public function doctype() {
-        if(QA_CACHING_STATUS && !qa_is_logged_in()) {
+        $main = new qa_caching_main;
+        if($main->do_caching()) {
             if(isset($this->content['notices']))
                 unset($this->content['notices']);
         }
@@ -15,7 +16,8 @@ class qa_html_theme_layer extends qa_html_theme_base {
     }
     public function head_script() {
         qa_html_theme_base::head_script();
-        if(QA_CACHING_STATUS && !qa_is_logged_in()) {
+        $main = new qa_caching_main;
+        if($main->do_caching()) {
             $this->output('<script src="'.qa_path('qa-plugin/q2a-caching/js/qa-caching.js', null, null, QA_URL_FORMAT_NEAT).'"></script>');
 			
         }
