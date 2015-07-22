926e79b1c498f5c63a96f869e28a99132a9fbdd0
sama55 <sama55@cmsbox.jp>
2015-07-22 16:47:41 +0900

    Update qa-caching-overrides.php

--- qa-plugin/q2a-caching/qa-caching-overrides.php
+++ qa-plugin/q2a-caching/qa-caching-overrides.php
@@ -11,29 +11,32 @@ that the code has not expired (if more than QA_FORM_EXPIRY_SECS have passed). Lo
 function qa_check_form_security_code($action, $value) {
     $main = new qa_caching_main;
     if($main->now_caching()) {
-		$reportproblems=array();
-		$silentproblems=array();
-		if (!isset($value))
-			$silentproblems[]='code missing';
-		elseif (!strlen($value))
-			$silentproblems[]='code empty';
-		else {
-			if (empty($silentproblems) && empty($reportproblems)) {
-				if($value != session_id())
-					$reportproblems[]='code mismatch';
-			}
-		}
-		if (count($reportproblems))
-			@error_log(
-				'PHP Question2Answer form security violation for '.$action.
-				' by '.(qa_is_logged_in() ? ('userid '.qa_get_logged_in_userid()) : 'anonymous').
-				' ('.implode(', ', array_merge($reportproblems, $silentproblems)).')'.
-				' on '.@$_SERVER['REQUEST_URI'].
-				' via '.@$_SERVER['HTTP_REFERER']
-			);
-		return (empty($silentproblems) && empty($reportproblems));
-    } else
+        $reportproblems=array();
+        $silentproblems=array();
+        if (!isset($value)) {
+            $silentproblems[]='code missing';
+        } else if (!strlen($value)) {
+            $silentproblems[]='code empty';
+        } else {
+            if (empty($silentproblems) && empty($reportproblems)) {
+                if($value != session_id()) {
+                    $reportproblems[]='code mismatch';
+                }
+            }
+        }
+        if (count($reportproblems)) {
+            @error_log(
+                'PHP Question2Answer form security violation for '.$action.
+                ' by '.(qa_is_logged_in() ? ('userid '.qa_get_logged_in_userid()) : 'anonymous').
+                ' ('.implode(', ', array_merge($reportproblems, $silentproblems)).')'.
+                ' on '.@$_SERVER['REQUEST_URI'].
+                ' via '.@$_SERVER['HTTP_REFERER']
+            );
+        }
+        return (empty($silentproblems) && empty($reportproblems));
+    } else {
         return qa_check_form_security_code_base($action, $value);
+    }
 }
 
 /*
