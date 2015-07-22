f434459b2f130bfedb003477414d43c50e61271a
sama55 <sama55@cmsbox.jp>
2015-07-22 16:43:07 +0900

    Update qa-caching-layer.php

--- qa-plugin/q2a-caching/qa-caching-layer.php
+++ qa-plugin/q2a-caching/qa-caching-layer.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
-	header('Location: ../../');
-	exit;
+    header('Location: ../../');
+    exit;
 }
 require_once QA_PLUGIN_DIR.'q2a-caching/qa-caching-main.php';
 
@@ -9,8 +9,9 @@ class qa_html_theme_layer extends qa_html_theme_base {
     public function doctype() {
         $main = new qa_caching_main;
         if($main->now_caching()) {
-            if(isset($this->content['notices']))
+            if(isset($this->content['notices'])) {
                 unset($this->content['notices']);
+            }
         }
         qa_html_theme_base::doctype();
     }
@@ -19,7 +20,6 @@ class qa_html_theme_layer extends qa_html_theme_base {
         $main = new qa_caching_main;
         if($main->now_caching()) {
             $this->output('<script src="'.qa_path('qa-plugin/q2a-caching/js/qa-caching.js', null, null, QA_URL_FORMAT_NEAT).'"></script>');
-			
         }
     }
 }
