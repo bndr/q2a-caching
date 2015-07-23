a187a2be359a255572d4a0aeb211b40027e1bb87
sama55 <sama55@cmsbox.jp>
2015-07-23 17:48:56 +0900

    Update qa-caching-overrides.php

--- qa-plugin/q2a-caching/qa-caching-overrides.php
+++ qa-plugin/q2a-caching/qa-caching-overrides.php
@@ -7,12 +7,16 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 /*
 Return the user identification cookie sent by the browser for this page request, or null if none
 */
-function qa_cookie_get() {
-    $main = new qa_caching_main;
-    if($main->now_caching()) {
-        return null;
+function qa_cookie_get($ignorcache=false) {
+    if($ignorcache) {
+        return qa_cookie_get_base();
+    } else {
+        $main = new qa_caching_main;
+        if($main->now_caching()) {
+            return null;
+        }
+        return qa_cookie_get_base();
     }
-    return qa_cookie_get_base();
 }
 
 /*
