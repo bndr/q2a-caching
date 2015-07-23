23f6c1718340d1bba0f5b3e92413c5ff7156da4c
sama55 <sama55@cmsbox.jp>
2015-07-23 09:53:22 +0900

    Update qa-plugin.php

--- qa-plugin/q2a-caching/qa-plugin.php
+++ qa-plugin/q2a-caching/qa-plugin.php
@@ -41,7 +41,4 @@ qa_register_plugin_layer(
     'qa-caching-layer.php', // PHP file containing module class
     'q2a Caching Plugin Layer'
 );
-qa_register_plugin_overrides(
-    'qa-caching-overrides.php' // PHP file containing overrided function
-);
 
